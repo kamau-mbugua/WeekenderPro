f4b220cc2b5c3be26e128083ee61040aafb803ba
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-16 22:43:07 +0300

    Partial Implementation of Adapter Class

--- app/src/main/java/com/example/weekenderpro/EventListActivity.java
+++ app/src/main/java/com/example/weekenderpro/EventListActivity.java
@@ -1,13 +1,21 @@
 package com.example.weekenderpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.List;
 
 public class EventListActivity extends AppCompatActivity {
 
@@ -16,6 +24,12 @@ public class EventListActivity extends AppCompatActivity {
     private FirebaseStorage storage;
     private DatabaseReference mDatabase;
     private ValueEventListener mDBListener;
+    List<Events> mEvents;
+    List<Events> filteredmEvents;
+    LinearLayoutManager layoutManager;
+    EditText searchText;
+    ProgressBar circleP_bar;
+    TextView defaultView;
 
 
 
@@ -23,5 +37,39 @@ public class EventListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        recyclerView = findViewById(R.id.recyclerVw);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        defaultView = findViewById(R.id.defaultView);
+
+        searchText = findViewById(R.id.editTextSearch);
+
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().isEmpty()) {
+
+//                    productSearch(s.toString());
+                    settAdapter(editable.toString());
+                } else {
+                    mAdapter = new RecyclerAdapter(EventListActivity.this, mEvents);
+                    recyclerView.setAdapter(mAdapter);
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/weekenderpro/RecyclerAdapter.java
+++ app/src/main/java/com/example/weekenderpro/RecyclerAdapter.java
@@ -1,4 +1,126 @@
 package com.example.weekenderpro;
 
-public class RecyclerAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public  class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
+    private static final String TAG = "hotelAdapter";
+    private Context mContext;
+    private List<Events> eventss;
+    // private OnItemClickListener mListener;
+
+    public RecyclerAdapter(Context context, List<Events> uploads) {
+        mContext = context;
+        eventss = uploads;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder:new View ...");
+        View v = LayoutInflater.from(mContext).inflate(R.layout.row_layout_home, parent, false);
+        RecyclerViewHolder hotelView = new RecyclerViewHolder(v);
+
+        Log.d(TAG, "onCreateViewHolder: view created...");
+        return hotelView;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: align to recycler...");
+        final Events currentHotel = eventss.get(position);
+
+        holder.hotelLocation.setText("Location :" + currentHotel.getHotelLocation());
+        holder.hotelName.setText("Hotel Name :" + currentHotel.getHotelName());
+        holder.ratings.setText("Ratings :" + currentHotel.getHotelRating());
+        holder.tagsList.setText("TagList :" + currentHotel.getHotelListTag());
+        holder.tvPrice.setText("Price per Day: Ksh."+" "+currentHotel.getHotelPricePerHour());
+        holder.tvMapUrl.setText(currentHotel.getMapUrl());
+        holder.tvPhone.setText(currentHotel.getPhone());
+        holder.tvemail.setText(currentHotel.getEmail());
+        holder.tvwebsite.setText(currentHotel.getWebsiteUrl());
+
+
+        Picasso.get()
+                .load(currentHotel.getImageUri())
+                .placeholder(R.drawable.placeholder)
+                .fit()
+                .centerCrop()
+                .into(holder.hotelImage);
+
+
+        holder.clickedLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent passIntent = new Intent(mContext, HotelDetail.class);
+                passIntent.putExtra("hotelLocation1", currentHotel.getHotelLocation());
+                passIntent.putExtra("hotelName1", currentHotel.getHotelName());
+                passIntent.putExtra("hotelRating1", currentHotel.getHotelRating());
+                passIntent.putExtra("hotelListTag1", currentHotel.getHotelListTag());
+                passIntent.putExtra("imageUri1", currentHotel.getImageUri());
+                passIntent.putExtra("email1", currentHotel.getImageUri());
+                passIntent.putExtra("phone1", currentHotel.getImageUri());
+                passIntent.putExtra("mapUrl1", currentHotel.getImageUri());
+                passIntent.putExtra("websiteUrl1", currentHotel.getImageUri());
+                passIntent.putExtra("hotelPricePerHour1", currentHotel.getHotelPricePerHour());
+
+
+                mContext.startActivity(passIntent);
+                Log.d(TAG, "onClick: detail view...");
+            }
+        });
+        Log.d(TAG, "onBindViewHolder: done binding....");
+    }
+
+    @Override
+    public int getItemCount() {
+        //return hotels.size();
+
+        Log.d(TAG, "getItemCount: Counting method...");
+        if (eventss != null) {
+            Log.d(TAG, "getItemCount: Done counting for list...");
+            return eventss.size();
+        }
+        Log.d(TAG, "getItemCount: Done counting for non-list...");
+        return 0;
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView ratings, hotelLocation, hotelName, tagsList, tvPhone, tvemail,tvwebsite,tvMapUrl,tvPrice;
+        public ImageView hotelImage;
+        CardView clickedLayout;
+
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            ratings = itemView.findViewById(R.id.ratings);
+            hotelLocation = itemView.findViewById(R.id.hotelLocation);
+            hotelName = itemView.findViewById(R.id.hotelName);
+            tagsList = itemView.findViewById(R.id.tagsList);
+            hotelImage = itemView.findViewById(R.id.hotelImage);
+            clickedLayout = itemView.findViewById(R.id.hotelCard);
+            tvMapUrl = itemView.findViewById(R.id.tvMapUrl);
+            tvPhone= itemView.findViewById(R.id.tvPhone);
+            tvemail=itemView.findViewById(R.id.tvemail);
+            tvwebsite = itemView.findViewById(R.id.tvwebsite);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+
+
+
+        }
+    }
 }
