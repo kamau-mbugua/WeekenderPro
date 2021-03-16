a83d1a85f7364be850d76988105c37a79ba05b07
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-16 22:02:19 +0300

    Staged the components

--- app/src/main/java/com/example/weekenderpro/MainActivity.java
+++ app/src/main/java/com/example/weekenderpro/MainActivity.java
@@ -3,12 +3,20 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
+ public class MainActivity extends AppCompatActivity {
+     ExtendedFloatingActionButton extended_fab;
+     Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        extended_fab= findViewById(R.id.extended_fab);
+        btnSearch = findViewById(R.id.btnSearch);
     }
 }
