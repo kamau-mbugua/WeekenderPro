f7577b04a831858b30a3d8feb4fbe8af15045f43
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-16 22:17:44 +0300

    RecyclerAdapter added

--- app/src/main/java/com/example/weekenderpro/EventListActivity.java
+++ app/src/main/java/com/example/weekenderpro/EventListActivity.java
@@ -1,10 +1,21 @@
 package com.example.weekenderpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
 public class EventListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerAdapter mAdapter;
+    private FirebaseStorage storage;
+    private DatabaseReference mDatabase;
+    private ValueEventListener mDBListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/weekenderpro/RecyclerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.weekenderpro;
+
+public class RecyclerAdapter {
+}
