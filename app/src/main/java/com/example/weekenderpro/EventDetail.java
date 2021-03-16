1d2d63f54ef01c818b5d5a3fd4ffd4d8aa7a2a6f
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-17 01:04:03 +0300

    renamed the event detail activity

--- app/src/main/java/com/example/weekenderpro/EventDetail.java
+++ app/src/main/java/com/example/weekenderpro/EventDetail.java
@@ -9,6 +9,6 @@ public class EventDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hotel_detail);
+        setContentView(R.layout.activity_event_detail);
     }
 }
