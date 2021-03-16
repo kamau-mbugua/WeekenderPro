1796c7a92c66fd34f46f98582181db07255b97fd
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-16 22:04:50 +0300

    Implemented the view button

--- app/src/main/java/com/example/weekenderpro/MainActivity.java
+++ app/src/main/java/com/example/weekenderpro/MainActivity.java
@@ -2,7 +2,9 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,13 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
         setContentView(R.layout.activity_main);
         extended_fab= findViewById(R.id.extended_fab);
         btnSearch = findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HotelListActivity.class));
+                finish();
+            }
+        });
     }
 }
